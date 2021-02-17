fff0cd65775e2cabb749f18d6040a9dbabfec851
Kenneth <fossen.kenneth@gmail.com>
2021-02-17 10:27:06 +0100

    Backend init commit

---
+++ lop-backend/src/main/java/no/hvl/dat251/gr9/lopbackend/LopBackendApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat251.gr9.lopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LopBackendApplication.class, args);
+	}
+
+}
---
+++ lop-backend/src/test/java/no/hvl/dat251/gr9/lopbackend/LopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat251.gr9.lopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
