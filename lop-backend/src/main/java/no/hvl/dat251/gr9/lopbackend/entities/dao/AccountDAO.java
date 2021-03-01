9377692518d71974e8c6aa50c346c03077bfe996
Andrè Jønland <andrefroseth@hotmail.com>
2021-03-01 22:05:51 +0100

    Additional commits that disappeared
    Two files did'nt make it to the previous commit, don't know why.

--- lop-backend/src/main/java/no/hvl/dat251/gr9/lopbackend/entities/dao/AccountDAO.java
+++ lop-backend/src/main/java/no/hvl/dat251/gr9/lopbackend/entities/dao/AccountDAO.java
@@ -1,14 +1,16 @@
-package no.hvl.dat251.gr9.lopbackend.repositories;
+package no.hvl.dat251.gr9.lopbackend.entities.dao;
 
-import no.hvl.dat251.gr9.lopbackend.entities.dao.AccountDAO;
+import no.hvl.dat251.gr9.lopbackend.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends JpaRepository<AccountDAO, Long> {
-    public AccountDAO findByUsername(String username);
-    public AccountDAO save(AccountDAO accounts);
-    public List<AccountDAO> findAll();
+public interface AccountDAO extends JpaRepository<Account, Long> {
+    public Account save(Account accounts);
+    public List<Account> getAll();
+    Optional<Account> getByEmail(String email);
+    Optional<Account> getById(String id);
 }
