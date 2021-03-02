d0af9f133ccf7a01c415ae79b3c306e6464d7bab
Rune Almåsbakk <runal_1998@hotmail.com>
2021-03-02 13:07:00 +0100

    RoleDAO

--- lop-backend/src/main/java/no/hvl/dat251/gr9/lopbackend/entities/dao/RoleDAO.java
+++ lop-backend/src/main/java/no/hvl/dat251/gr9/lopbackend/entities/dao/RoleDAO.java
@@ -1,2 +1,14 @@
-package no.hvl.dat251.gr9.lopbackend.entities.dao;public interface RoleDAO {
+package no.hvl.dat251.gr9.lopbackend.entities.dao;
+
+import no.hvl.dat251.gr9.lopbackend.entities.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleDAO extends JpaRepository<Roles, String> {
+
+    Optional<Roles> findByRole(String role);
+
 }
