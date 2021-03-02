5cc866f9e7905fffba47f55868544a9364177225
Kenneth <fossen.kenneth@gmail.com>
2021-03-02 19:29:07 +0100

    New header with NavBar and tests

--- lop-frontend/src/Header.tsx
+++ lop-frontend/src/Header.tsx
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Navbar, NavbarBrand, Nav} from 'react-bootstrap';
 declare module 'logo.png'
 
 
-export default class Header extends Component<{}> {
-    
+function Header() {
 
-
-    render() {
-    
-        return (
-            <div>
+    return (
+        <Navbar expand="lg" sticky="top">
+            <NavbarBrand>
                 <img src="logo.png" width="70px" height="70px" alt="" />
-                <span data-testid="text-content" >Dette er logo.png</span>
-            </div>
-        );
-    }
+            </NavbarBrand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#login">Login</Nav.Link>
+                    <Nav.Link href="#register">Register</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
+
+export default Header;
+
+/*<Container>
+            <Row>
+                <Image src="logo.png" ></Image>
+            </Row>
+            <div>
+                
+             <span data-testid="text-content" >Dette er logo.png</span>
+            </div>
+        </Container>*/
