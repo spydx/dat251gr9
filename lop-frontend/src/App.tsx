a898a8bbc194aea67df5c4b5cc059cd2cf157f25
Hans-Christian <31404215+H-C-H@users.noreply.github.com>
2021-03-29 21:06:48 +0200

    Refactor and improve previous commit

--- lop-frontend/src/App.tsx
+++ lop-frontend/src/App.tsx
@@ -1,43 +1,48 @@
 import React from "react";
 import { Button, Container } from "react-bootstrap";
 import { EventList } from "./components/EventList";
+import { LoadingSpinner } from "./components/Spinner";
 import Header from "./Header";
 import { fetchEventList } from "./services/api";
 import { Event } from './types'
 
-type MyState = {
-  events: Event[]
+type PageState = {
+  events: Event[] | undefined
 };
 
-export class App extends React.Component<{}, MyState> {
+export class App extends React.Component<{}, PageState> {
   constructor() {
     super({});
-    this.state = { events: [] }
+    this.state = { events: undefined }
   }
 
-  componentWillMount() {
-    const the_promise = fetchEventList();
-    the_promise.then(events => {
+  componentDidMount() {
+    this.refreshEventList()
+  }
+  
+  refreshEventList() {
+    // clear the previous state, sets a loading indicator
+    this.setState({ events: undefined })
+    fetchEventList().then(events => {
       if (events !== undefined) {
         this.setState({ events })
       } else {
-        console.log("had an error fetching state")
+        console.log("App::update(): events == undefined")
       }
     })
   }
 
   render() {
-    function update() {
-      console.log("update called, but not implemented");
-    }
-
-    const eventList = this.state.events;
+    const eventList = this.state.events
+      ? <EventList id={1} events={this.state.events} />
+      : <LoadingSpinner />;
 
     return (
       <Container className="text-center">
         <Header />
-        <EventList id={1} events={eventList} />
-        <Button onClick={update}>Update</Button>
+        <Button onClick={() => this.refreshEventList()}>Update</Button>
+        <br/>
+        {eventList}
         <div className="text-muted">DAT251 Gruppe9</div>
       </Container>
     );
---
+++ lop-frontend/src/components/Spinner.tsx
@@ -0,0 +1,5 @@
+export const LoadingSpinner = () =>
+    <div className="spinner-border" role="status">
+        <span className="sr-only">Loading...</span>
+    </div>
+
