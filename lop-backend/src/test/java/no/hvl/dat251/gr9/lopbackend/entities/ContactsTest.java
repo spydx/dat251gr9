e1747b8be8be2fc176b31c049a35aa478b66c306
Rune Almåsbakk <runal_1998@hotmail.com>
2021-04-01 20:44:09 +0200

    Added content to contacts test file

--- lop-backend/src/test/java/no/hvl/dat251/gr9/lopbackend/entities/ContactsTest.java
+++ lop-backend/src/test/java/no/hvl/dat251/gr9/lopbackend/entities/ContactsTest.java
@@ -1,14 +1,14 @@
-package no.hvl.dat251.gr9.lopbackend.entities.dto;
+package no.hvl.dat251.gr9.lopbackend.entities;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ContactsDTOTest {
+public class ContactsTest {
 
     @Test
-    public void contactsDTOGetSetTest(){
-        var contact = new ContactsDTO();
+    public void contactsGetSetTest(){
+        var contact = new Contacts();
 
         var email = "testemail@test.no";
         var name = "test testesen";
