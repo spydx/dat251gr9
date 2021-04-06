75f23795f388b63195233dcd20d2b9c8f5cf8d1d
Kenneth <fossen.kenneth@gmail.com>
2021-04-06 10:41:47 +0200

    Cypress E2E integration test setup (initial)

--- lop-frontend/src/Footer.tsx
+++ lop-frontend/src/Footer.tsx
@@ -1,9 +1,7 @@
 // todo: should be at the bottom of the page
 function Footer() {
   return (
-    <footer className="border-top container">
       <div className="text-muted">DAT251 Gruppe9</div>
-    </footer>
   );
 }
 
