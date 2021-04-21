f3d45f19c2bb73fd25ccf6710862140242fcd97d
Hans-Christian <31404215+H-C-H@users.noreply.github.com>
2021-04-21 10:23:54 +0200

    Handle errors on sign in attempt

--- lop-frontend/src/pages/SignInPage.tsx
+++ lop-frontend/src/pages/SignInPage.tsx
@@ -1,6 +1,8 @@
-import { Button, Card, Form, Row } from "react-bootstrap";
+import { Alert, Button, Card, Col, Form, Row } from "react-bootstrap";
 import MasterPage from "../MasterPage";
-import { ApiPath, doPost } from "../services/api";
+import { ApiPath, doPost, NetworkError, ResponseError } from "../services/api";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
 
 /** sends the login request and saves the token */
 async function performLogin(email: string, password: string) {
@@ -11,45 +13,70 @@ async function performLogin(email: string, password: string) {
   localStorage.setItem("token", token);
 }
 
-// TODO: form validation / errors
 export const SignInPage: React.FunctionComponent = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const history = useHistory();
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const formData = event.currentTarget;
-    await performLogin(formData["formEmail"].value, formData["formPassword"].value);
-    alert("signed in with token " + localStorage.getItem("token"));
+    try {
+      await performLogin(formData["formEmail"].value, formData["formPassword"].value);
+      history.push('/'); // TODO: for now, we just always redirect to home
+    } catch (e) {
+      if (e instanceof ResponseError) {
+        if (e.status === 401) {
+          setErrorMessage("Invalid username or password");
+        } else {
+          setErrorMessage("Unknown error: " + e.status);
+        }
+      } else if (e instanceof NetworkError) {
+        setErrorMessage("Unable to connect to server");
+      } else {
+        throw e;
+      }
+    }
   };
+
+  let errorBanner = errorMessage ? (
+    <Alert variant="danger" dismissible onClose={() => setErrorMessage("")}>
+      {errorMessage}
+    </Alert>
+  ) : null;
 
   return (
     <MasterPage>
       <h2>Sign In</h2>
       <Row className="justify-content-center">
-        <Card>
-          <Card.Body>
-            <Form noValidate={false} className="signInForm" onSubmit={handleSubmit}>
-              <Form.Group controlId="formEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control name="email" required type="email" placeholder="Enter email" />
-              </Form.Group>
+        <Col xs="auto">
+          {errorBanner}
+          <Card>
+            <Card.Body>
+              <Form noValidate className="signInForm" onSubmit={handleSubmit}>
+                <Form.Group controlId="formEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control name="email" required type="email" placeholder="Enter email" />
+                </Form.Group>
 
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control name="password" required type="password" placeholder="Password" />
-              </Form.Group>
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control name="password" required type="password" placeholder="Password" />
+                </Form.Group>
 
-              {/* Enhancement: add a "remember me" checkbox
+                {/* Enhancement: add a "remember me" checkbox
               <Form.Group controlId="formCheckbox">
                 <Form.Check type="checkbox" label="Remember me" />
               </Form.Group> */}
-              <div className="text-right">
-                <Button variant="primary" type="submit">
-                  Sign In
-                </Button>
-              </div>
-            </Form>
-          </Card.Body>
-        </Card>
+                <div className="text-right">
+                  <Button variant="primary" type="submit">
+                    Sign In
+                  </Button>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </MasterPage>
   );
--- lop-frontend/src/services/api.ts
+++ lop-frontend/src/services/api.ts
@@ -7,31 +7,59 @@ export const ApiPath = {
   Event: (id: string) => "/events/" + id,
 };
 
-/*
-const APPLICATIONJSON : string = "application/json"
-const CONTENTTYPE : string = "Content-Type"
-const HTTP_POST: string = "POST"
-const HTTP_GET: string = "GET"
-const CORS_ACAO = 'access-control-allow-origin' // *
-const CORS_ACAC = 'access-control-allow-credentials' // true
-*/
+// Some error classes
 
-export const authDoGet = (path: string): Promise<any> => {
+export class ResponseError extends Error {
+  public status: number;
+
+  constructor(status: number) {
+    super("");
+    this.status = status;
+  }
+}
+
+export class NetworkError extends Error {
+  constructor() {
+    super("");
+  }
+}
+
+//
+
+function robustFetch(input: RequestInfo, init?: RequestInit) {
+  const handleNetworkError = (whatsThis: any) => {
+    console.log("got", whatsThis);
+    return Promise.reject(new NetworkError());
+  };
+  const handleResponse = (response: Response) => {
+    if (response.ok) {
+      return response.json().then((data) => ({ data, status: response.status }));
+    } else {
+      return Promise.reject(new ResponseError(response.status));
+    }
+  };
+  return fetch(input, init).catch(handleNetworkError).then(handleResponse);
+}
+
+//
+
+export const authDoGet = async (path: string): Promise<any> => {
   const token = localStorage.getItem("token");
-  return fetch(`${BACKEND_ROOT}${path}`, {
+  const resp = await fetch(`${BACKEND_ROOT}${path}`, {
     headers: { Autorization: "Bearer " + token },
-  }).then((resp) => resp.json());
-}
+  });
+  return resp.json();
+};
 
-export const doGet = (path: string): Promise<any> => {
-  return fetch(`${BACKEND_ROOT}${path}`).then((resp) => resp.json());
-}
+export const doGet = async (path: string): Promise<any> => {
+  const resp = await fetch(`${BACKEND_ROOT}${path}`);
+  return resp.json();
+};
 
-// TODO: error handling
 export const doPost = (path: string, requestBody: any): Promise<any> => {
-  return fetch(`${BACKEND_ROOT}${path}`, {
+  return robustFetch(`${BACKEND_ROOT}${path}`, {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify(requestBody),
-  }).then((resp) => resp.json());
-}
+  });
+};
