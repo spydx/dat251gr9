ad1323391ded0ea491176bb36a25d3889a6bce46
Hans-Christian <31404215+H-C-H@users.noreply.github.com>
2021-04-27 12:05:57 +0200

    Upgrade Springfox/Swagger

--- lop-backend/src/main/java/no/hvl/dat251/gr9/lopbackend/config/ConfigSwagger.java
+++ lop-backend/src/main/java/no/hvl/dat251/gr9/lopbackend/config/ConfigSwagger.java
@@ -2,35 +2,24 @@ package no.hvl.dat251.gr9.lopbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 // URL -----> http://localhost:8080/swagger-ui.html
 
 @Configuration
-@EnableSwagger2
 public class ConfigSwagger {
-
     @Bean
     public Docket api() {
-        List<SecurityScheme> schemeList = new ArrayList<>();
-        schemeList.add(new ApiKey(HttpHeaders.AUTHORIZATION, "Bearer", "header"));
         return new Docket(DocumentationType.SWAGGER_2)
                 .produces(Collections.singleton("application/json"))
                 .consumes(Collections.singleton("application/json"))
-                .securitySchemes(schemeList)
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("no.hvl.dat251.gr9.lopbackend"))
@@ -39,13 +28,11 @@ public class ConfigSwagger {
                 .apiInfo(apiInfo());
     }
 
+
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "LOP REST API",
-                "LOP API",
-                "API Production",
-                "Terms Of service",
-                new Contact("Lop Admin", "www.lop.no", "support@lop.no"),
-                "License of API", "API license URL", Collections.emptyList());
+        return new ApiInfoBuilder()
+                .title("LOP REST API")
+                .description("LOP API")
+                .build();
     }
 }
